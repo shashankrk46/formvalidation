bf44f666b628c882289f906ed39791d7342cadc4
shashankrk46 <shashankrk46@gmail.com>
2020-03-11 13:11:44 +0530

    validation done

--- app.js
+++ app.js
@@ -19,11 +19,37 @@ if(!re.test(name.value)){
 }
 }
 function validateZip(){
+    const zip=document.getElementById('zip');
+const re=/^[0-9]{6}(-[0-9]{4})?$/;
+
+if(!re.test(zip.value)){
+   zip.classList.add('is-invalid');
+}else{
+    zip.classList.remove('is-invalid');
 
 }
+}
 function validateEmail(){
+    const email=document.getElementById('email');
+const re=/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+if(!re.test(email.value)){
+   email.classList.add('is-invalid');
+}else{
+    email.classList.remove('is-invalid');
+
+}
 
 }
 function validatePhone(){
+    const phone=document.getElementById('phone');
+    const re=/^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+    
+    if(!re.test(phone.value)){
+       phone.classList.add('is-invalid');
+    }else{
+        phone.classList.remove('is-invalid');
+    
+    }
 
 }
