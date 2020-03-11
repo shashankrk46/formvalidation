a0d022c76114369690f628b5a05984f651138c62
shashankrk46 <shashankrk46@gmail.com>
2020-03-11 11:58:02 +0530

    added file

---
+++ app.js
@@ -0,0 +1,29 @@
+//form blur evnt listners 
+document.getElementById('name').addEventListener('blur',
+validateName);
+document.getElementById('zip').addEventListener('blur',
+validateZip);
+document.getElementById('email').addEventListener('blur',
+validateEmail);
+document.getElementById('phone').addEventListener('blur',
+validatePhone);
+
+function validateName(){
+const name=document.getElementById('name');
+const re=/^[a-zA-Z]{2,10}$/;
+
+if(!re.test(name.value)){
+   name.classList.add('is-invalid');
+}else{
+    name.classList.remove('is-invalid');
+}
+}
+function validateZip(){
+
+}
+function validateEmail(){
+
+}
+function validatePhone(){
+
+}
